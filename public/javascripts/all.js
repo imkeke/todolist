e86131c4c7a7042f02269b4bd682b4620e20769f
keke <zhengkefeng15@gmail.com>
2012-12-28 16:20:51 +0800

    基本功能完成

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -9,6 +9,32 @@ $(function() {
         item: _this.prev().val()
       }
     }).done(function(res) {
+      if (res.succ) {
+        if ($('tbody').find('tr').length) {
+          var id = parseInt($('tbody').find('tr').last().data('id')) + 1;
+        } else {
+          var id = 0;
+        }
+        var html = $('<tr data-id="'+id+'"><td>'+id+'</td><td>'+res.data+'</td><td><a href="javascript:;" class="btn btn-mini btn-primary h-del">已完成</a></td></tr>');
+        $('tbody').append(html);
+        _this.prev().val('');
+      }
+    });
+  });
+
+  $('body').on('click', '.h-del', function() {
+    var _this = $(this);
+    $.ajax({
+      url: '/del',
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        item: _this.parent().prev().text()
+      }
+    }).done(function(res) {
+      if (res.succ) {
+        window.location.reload();
+      }
     });
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,41 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('db.db');
 
-/*
- * GET home page.
- */
+exports.index = function(req, res) {
+    var lists = [];
+    db.serialize(function() {
+        db.each("SELECT name FROM Todolist", function(err, row) {
+            lists.push(row.name);
+        }, function() {
+            res.render('index', {
+                title: 'todolist',
+                lists: lists
+            });
+        });
+    });
 
-exports.index = function(req, res){
-  res.render('index', {
-    title: 'todolist',
-    lists: [{number: '1', name: '买菜', id: '123'},{number: '2', name: '做饭', id: '123'}]
-  });
 };
 
 exports.add = function(req, res) {
-    res.json({
-        succ: true,
-        item: req.param('item')
+    var item = req.param('item');
+    db.serialize(function() {
+        db.run("INSERT INTO Todolist VALUES (1, '" + item + "')", function() {
+            res.json({
+                "succ": true,
+                "data": item
+            });
+        });
+    });
+};
+
+exports.del = function(req, res) {
+    var item = req.param('item');
+    db.serialize(function() {
+        db.run("DELETE FROM Todolist WHERE name = '" + item + "'", function() {
+            res.json({
+                "succ": true,
+                "data": item
+            });
+        });
     });
 };
